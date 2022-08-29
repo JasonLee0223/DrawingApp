e0b72ec455a40774898f2bd7da2e852597a35534
Jason <ggj00125@gmail.com>
2022-08-29 17:16:14 +0900

    Feat: Point 객체 구현

---
+++ DrawingApp/DrawingApp/Model/Point.swift
@@ -0,0 +1,31 @@
+//
+//  Point.swift
+//  DrawingApp
+//
+//  Created by Jason on 2022/08/29.
+//
+
+import Foundation
+
+class Point {
+    
+    private(set) var x: Double
+    private(set) var y: Double
+    
+    init(x: Double, y: Double) {
+        self.x = x
+        self.y = y
+    }
+    
+    static func randomPoint() -> Point{
+        let randomX = Double.random(in: 0.0...900.0)
+        let randomY = Double.random(in: 0.0...650.0)
+        return Point(x: randomX, y: randomY)
+    }
+}
+
+extension Point:CustomStringConvertible {
+    var description: String {
+        return "Point: X=\(self.x), Y=\(self.y)"
+    }
+}
