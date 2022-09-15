0ade80214cdccf5dca815493113cd9bf637483d8
Jason <ggj00125@gmail.com>
2022-09-15 14:52:08 +0900

    Feat: 사각형이 그려질 DrawingView 생성

---
+++ DrawingApp/DrawingApp/View/DrawingView.swift
@@ -0,0 +1,21 @@
+//
+//  DrawingView.swift
+//  DrawingApp
+//
+//  Created by Jason on 2022/09/15.
+//
+
+import UIKit
+
+class DrawingView:UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        
+    }
+}
