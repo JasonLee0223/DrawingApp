81d78ea46e407813082ba2f6071541dc82b1ca3e
Jason <ggj00125@gmail.com>
2022-10-05 17:28:51 +0900

    Feat: Concrete Creator 역할을 하는 RectangleFactory 파일 생성 및 구현
    기본 팩토리 메서드를 갖게끔 RectangleViewProtocol을 준수하여 서로 다른 Product 객체를 만들도록한다.

---
+++ DrawingApp/DrawingApp/Model/Concrete Creator/RectagleFactory.swift
@@ -0,0 +1,15 @@
+//
+//  RectagleFactory.swift
+//  DrawingApp
+//
+//  Created by Jason on 2022/10/05.
+//
+
+import Foundation
+
+// Concrete Creator
+class RectangleFactory: RectangleViewProtocol {
+    func createRandomRectangle() -> CreateRandomRect {
+        return generateRectangle()
+    }
+}
