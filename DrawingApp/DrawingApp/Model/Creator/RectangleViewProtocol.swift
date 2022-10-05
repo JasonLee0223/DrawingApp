e4ee7be73f7525524f1e30e76c4eb8ea34c354f7
Jason <ggj00125@gmail.com>
2022-10-05 17:06:01 +0900

    Feat: Creator에 해당하는 RectangleViewProtocol 그룹 및 파일 생성

---
+++ DrawingApp/DrawingApp/Model/Creator/RectangleViewProtocol.swift
@@ -0,0 +1,13 @@
+//
+//  RectangleViewProtocol.swift
+//  DrawingApp
+//
+//  Created by Jason on 2022/10/05.
+//
+
+import Foundation
+
+// Creator
+protocol RectangleViewProtocol {
+    func createRandomRectangle() -> CreateRandomRect
+}
