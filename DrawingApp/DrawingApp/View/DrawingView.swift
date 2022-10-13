989d31194d39b8ceb268a6d5bfbca3fe9c93ef5c
Jason <ggj00125@gmail.com>
2022-10-14 06:31:22 +0900

    Feat: ButtonAction 생성 및 직접 Frame을 지정하지 않는 방향으로 수정

--- DrawingApp/DrawingApp/View/DrawingView.swift
+++ DrawingApp/DrawingApp/View/DrawingView.swift
@@ -6,16 +6,27 @@
 //
 
 import UIKit
+import OSLog
 
-class DrawingView:UIView {
+protocol DrawingViewDelegate: AnyObject{
+    func creatingRectangleRequested()
     
-    override init(frame: CGRect) {
-        super.init(frame: CGRect(x: 0, y: 0, width: 1166, height: 1024))
+}
+
+class DrawingView: UIView {
+    
+    weak var delegate: DrawingViewDelegate?
+    
+    init(frame: CGRect, backgroundColor: UIColor) {
+        super.init(frame: frame)
+        self.backgroundColor = backgroundColor
         layout()
+        setGeneratingButtonAction()
     }
     
     required init?(coder: NSCoder) {
         super.init(coder: coder)
+        fatalError("init(coder:) has not been implemented")
         
     }
     
@@ -23,19 +34,34 @@ class DrawingView:UIView {
         var config = UIButton.Configuration.gray()
         config.buttonSize = .large
         config.contentInsets = NSDirectionalEdgeInsets(top: 16, leading: 16, bottom: 16, trailing: 16)
-        
+
         var titleAttr = AttributedString.init(" ▢ ")
         titleAttr.font = .systemFont(ofSize: 35.0, weight: .heavy)
         config.attributedTitle = titleAttr
-        
+
         var subtitleAttr = AttributedString.init(" 사각형 ")
         subtitleAttr.font = .systemFont(ofSize: 15.0, weight: .regular)
         config.attributedSubtitle = subtitleAttr
-        
+
+//        let rectButton = UIButton(configuration: config, primaryAction: UIAction(handler: addAction))
         let rectButton = UIButton(configuration: config)
         rectButton.frame = CGRect(x: 583, y: 930, width: 150, height: 93)
         return rectButton
     }()
+    
+    //MARK: - Button Action
+    private func setGeneratingButtonAction() {
+        let buttonAction: UIAction = UIAction(title: ""){ _ in
+            self.sendCreatingRectangleRequest()
+        }
+        self.createRectButton.addAction(buttonAction, for: .touchDown)
+    }
+    
+    private func sendCreatingRectangleRequest() {
+        if let delegate = self.delegate {
+            delegate.creatingRectangleRequested()
+        }
+    }
     
 }
 
