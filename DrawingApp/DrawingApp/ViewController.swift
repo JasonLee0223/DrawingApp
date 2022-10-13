638b04ca116fbd9c589ed06e553872c35eea7a4f
Jason <ggj00125@gmail.com>
2022-10-13 23:26:01 +0900

    Feat: SubView 추가 리펙토링 및 DrawingViewDelegate 구현

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -10,18 +10,60 @@ import OSLog
 
 class ViewController: UIViewController {
     
-    let drawingView = DrawingView()
-    let controlView = ControlView()
+    var drawingView: DrawingView?
+    var controlView: ControlView?
     let screenWidth = UIScreen.main.bounds.size.width
     let screenHeight = UIScreen.main.bounds.size.height
     
-    let testFactory = Plane()
+    private var plane: Plane = Plane()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.addSubview(drawingView)
-        self.view.addSubview(controlView)
+        initializeAllSubViews()
         
-        let rectangle = testFactory.order(factory: RectangleFactory())
+//        let rectangle = testFactory.addTo(rectFactory: RectangleFactory())
+//        let cnt = testFactory.countOfRectangle()
+//        print("\(cnt)")
     }
+    
+    //MARK: - [Confirm] Configure about UIView (SubViews)
+    private func initializeAllSubViews() {
+        setDrawingView()
+        setControlView()
+    }
+    
+    private func setDrawingView() {
+        let frame = CGRect(x: self.view.frame.minX,
+                           y: self.view.frame.minY,
+                           width: self.view.frame.width*0.8,
+                           height: self.view.frame.height)
+        let drawingView = DrawingView(frame: frame)
+        drawingView.delegate = self
+        self.view.addSubview(drawingView)
+    }
+    
+    private func setControlView() {
+        guard let drawingView = self.controlView else { return }
+        let frame = CGRect(x: drawingView.frame.width,
+                           y: self.view.frame.minY,
+                           width: self.view.frame.width - drawingView.frame.width,
+                           height: self.view.frame.height)
+        let controlView = ControlView(frame: frame)
+//        controlView.delegate = self
+        self.controlView = controlView
+        self.view.addSubview(controlView)
+    }
+    
+    //MARK: - [Excepted] Configure about TapGesture
+}
+
+//MARK: - DrawingViewDelegate
+extension ViewController: DrawingViewDelegate {
+    
+    func creatingRectangleRequested() {
+        guard let drawingView = self.drawingView else { return }
+        let rectangle = RectangleFactory.createRandomRectangle().generateRandomRectangle()
+        plane.addTo(rectFactory: rectangle)
+    }
+    
 }
