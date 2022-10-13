a744e676c2d13231890c3cc5e1414d8c3c7fd87f
Jason <ggj00125@gmail.com>
2022-10-14 06:32:10 +0900

    Feat: Delegate를 사용하여 RectangleView를 생성하는 메서드 구현 및 subview의 frame 설정

--- DrawingApp/DrawingApp/ViewController.swift
+++ DrawingApp/DrawingApp/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initializePlaneDelegate()
         initializeAllSubViews()
         
 //        let rectangle = testFactory.addTo(rectFactory: RectangleFactory())
@@ -37,21 +38,27 @@ class ViewController: UIViewController {
                            y: self.view.frame.minY,
                            width: self.view.frame.width*0.8,
                            height: self.view.frame.height)
-        let drawingView = DrawingView(frame: frame)
+        let drawingView = DrawingView(frame: frame, backgroundColor: .lightGray)
         drawingView.delegate = self
+        self.drawingView = drawingView
         self.view.addSubview(drawingView)
     }
     
     private func setControlView() {
-        guard let drawingView = self.controlView else { return }
+        guard let drawingView = self.drawingView else { return }
         let frame = CGRect(x: drawingView.frame.width,
                            y: self.view.frame.minY,
                            width: self.view.frame.width - drawingView.frame.width,
                            height: self.view.frame.height)
-        let controlView = ControlView(frame: frame)
+        let controlView = ControlView(frame: frame, backgroundColor: UIColor.systemGray6)
 //        controlView.delegate = self
         self.controlView = controlView
         self.view.addSubview(controlView)
+    }
+    
+    //MARK: - [Confirm] Configure about PlaneDelegate
+    private func initializePlaneDelegate() {
+        self.plane.delegate = self
     }
     
     //MARK: - [Excepted] Configure about TapGesture
@@ -61,9 +68,25 @@ class ViewController: UIViewController {
 extension ViewController: DrawingViewDelegate {
     
     func creatingRectangleRequested() {
-        guard let drawingView = self.drawingView else { return }
-        let rectangle = RectangleFactory.createRandomRectangle().generateRandomRectangle()
-        plane.addTo(rectFactory: rectangle)
+        guard self.drawingView != nil else { return }
+        let rectangleFactory = RectangleFactory().createRandomRectangle()
+        let rectangle = rectangleFactory.generateRandomRectangle()
+        plane.add(theCreated: rectangle)
     }
+}
+
+//MARK: - ControlViewDelegate
+extension ViewController: ControlViewDelegate {
     
+}
+
+//MARK: - PlaneDelegate
+extension ViewController: PlaneDelegate {
+    
+    func addingRectangleComplted(product: Rectangle) {
+        guard self.drawingView != nil else { return }
+        os_log("\(product)")
+        let newRectangleView = ViewFactory.generateRectangleView(of: product)
+        self.view.addSubview(newRectangleView)
+    }
 }
