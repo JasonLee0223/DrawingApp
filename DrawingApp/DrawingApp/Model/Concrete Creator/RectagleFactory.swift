f2c52e2ee182a0eeaf78e328ff0eeb2149bbc168
Jason <ggj00125@gmail.com>
2022-10-14 06:28:06 +0900

    Chore: Factory 생성하는데 역할 설명
    Plane과 Factory의 역할군에 대해 계속 헷갈리는 부분이 있어 주석에 기재

--- DrawingApp/DrawingApp/Model/Concrete Creator/RectagleFactory.swift
+++ DrawingApp/DrawingApp/Model/Concrete Creator/RectagleFactory.swift
@@ -7,7 +7,9 @@
 
 import Foundation
 
-// Concrete Creator
+//MARK: - Factory (Concrete Creator)
+// RectangleFactory 객체를 통해 Rectangle객체를 만들면 된다.
+// Rectangle을 생성하는 부분은 DrawingView의 Button Action을 통해 만들어진다.
 class RectangleFactory: RectangleViewProtocol {
     func createRandomRectangle() -> ProductAboutRectangle {
         return generateRectangle()
