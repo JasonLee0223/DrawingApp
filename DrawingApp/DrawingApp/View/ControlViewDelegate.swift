8bf61ee2020ec332083ff5c03211134d8a6ace9e
Jason <ggj00125@gmail.com>
2022-10-20 16:21:23 +0900

    Feat: ControlViewDelegate 파일로 분리
    ControlView에 정의되어있었던 부분 이동

---
+++ DrawingApp/DrawingApp/View/ControlViewDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  ControlViewDelegate.swift
+//  DrawingApp
+//
+//  Created by 이건행 on 2022/10/20.
+//
+
+import Foundation
+
+protocol ControlViewDelegate: AnyObject {
+    func clearingSelectedRectangleInfoCompleted()
+    
+}
